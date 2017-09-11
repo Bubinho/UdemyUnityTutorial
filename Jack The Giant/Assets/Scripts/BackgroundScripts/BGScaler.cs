2f57fbd8d4e00b855c46567c216748a3ab8b548b
Bubes <daniel-topp@t-online.de>
2017-09-11 18:20:50 +0200

    Added Cloud/Player Prefabs. Added Background

---
+++ Jack The Giant/Assets/Scripts/BackgroundScripts/BGScaler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScaler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// use this to scale the background for multiple resolutions
+
+		SpriteRenderer sr = GetComponent<SpriteRenderer> ();
+		Vector3 tempScale = transform.localScale;
+		float width = sr.sprite.bounds.size.x;
+		float worldHeight = Camera.main.orthographicSize * 2.0f;
+
+		// (Screen.width / Screen.height) = aspectratio of screen 
+		// to determine worldWidth multiply by worldHeight
+		float worldWidth = worldHeight * Screen.width / Screen.height; 
+	
+		// to detremine scale worldWidth / spritewidth
+		tempScale.x = worldWidth / width;
+		transform.localScale = tempScale;
+	}
+}
---
+++ Jack The Giant/Assets/Scripts/PlayerScripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 8f;
+	public float maxVelocity = 4f;
+
+	private Rigidbody2D myBody;
+	private Animator anim;
+	private Vector3 startScale;
+
+
+
+	void Awake(){
+		myBody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+		startScale = transform.localScale;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	void FixedUpdate () {
+		PlayerMoveKeyBoard ();
+	}
+
+
+	void PlayerMoveKeyBoard(){
+		float forceX = 0f;
+		float vel = Mathf.Abs (myBody.velocity.x);
+
+		float h = Input.GetAxisRaw ("Horizontal");
+
+		if (h > 0) {
+			if (vel < maxVelocity) {// to avoid speeding up faster and faaster and faster ....
+				forceX = speed;
+			}
+			Vector3 temp = transform.localScale;
+			temp.x = startScale.x; 
+			transform.localScale = temp; // set face of player to right
+			anim.SetBool ("Walk", true); // start walking anim
+
+		} 
+		else if (h < 0) {
+			if (vel < maxVelocity) {
+				forceX = -speed;
+			}
+			Vector3 temp = transform.localScale;
+			temp.x = -startScale.x; 
+			transform.localScale = temp; // set face of player to right
+			anim.SetBool ("Walk", true); // start walking anim
+		}
+		else{
+			anim.SetBool ("Walk", false);// stop walking anim
+		}
+
+		myBody.AddForce (new Vector2(forceX, 0));
+	}
+}
