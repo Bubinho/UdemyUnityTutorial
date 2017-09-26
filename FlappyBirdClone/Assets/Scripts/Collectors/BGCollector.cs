d6708bbd131db7026216ca275fd4dcd9309b3c6a
Bubinho <bubes1@gmx.de>
2017-09-26 15:51:26 +0200

    Added BackgroundCollector. Added Pipes + Collector

---
+++ FlappyBirdClone/Assets/Scripts/Collectors/BGCollector.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGCollector : MonoBehaviour {
+
+	private GameObject[] backgrounds;
+	private GameObject[] grounds;
+
+	private float lastBGX;
+	private float lastGroundX;
+
+	void Awake(){
+		backgrounds = GameObject.FindGameObjectsWithTag ("Background");
+		grounds = GameObject.FindGameObjectsWithTag ("Ground");
+
+		lastBGX = backgrounds [0].transform.position.x;
+		lastGroundX = grounds [0].transform.position.x;
+
+		for (int i = 1; i < backgrounds.Length; i++) {
+			if(lastBGX < backgrounds[i].transform.position.x){
+				lastBGX = backgrounds [i].transform.position.x;
+			}
+		}
+
+		for (int i = 1; i < grounds.Length; i++) {
+			if(lastGroundX < grounds[i].transform.position.x){
+				lastGroundX = grounds[i].transform.position.x;
+			}
+		}
+	}
+
+
+	void OnTriggerEnter2D(Collider2D target){
+		if (target.tag == "Background") {
+			Vector3 temp = target.transform.position;
+			temp.x = lastBGX + target.bounds.size.x; // letzte Posiiotn + hälfte des colliders
+			target.transform.position = temp;
+			lastBGX = target.transform.position.x;
+		}
+
+		else if (target.tag == "Ground") {
+			Vector3 temp = target.transform.position;
+			temp.x = lastGroundX + target.bounds.size.x;
+			target.transform.position = temp;
+			lastGroundX = target.transform.position.x;
+		}
+
+
+	}
+}
---
+++ FlappyBirdClone/Assets/Scripts/Collectors/PipeCollector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeCollector : MonoBehaviour {
+
+	private GameObject[] pipeHolders;
+	private float distance = 2.5f;
+	private float lastPipesX;
+	private float pipeMin = -1.3f;
+	private float pipeMax = 1.3f;
+
+
+	void Awake () {
+		pipeHolders = GameObject.FindGameObjectsWithTag ("PipeHolder");
+		lastPipesX = pipeHolders [0].transform.position.x;
+
+		for(int i = 1; i < pipeHolders.Length; i++){
+
+			// check for last position
+			if(lastPipesX < pipeHolders [i].transform.position.x){
+				lastPipesX = pipeHolders [i].transform.position.x;
+			}
+
+			// do y variation stuff
+			Vector3 temp =  pipeHolders [i].transform.position;
+			temp.y = Random.Range (pipeMin, pipeMax);
+			pipeHolders [i].transform.position = temp;
+
+
+
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D target)
+	{
+		if (target.tag == "PipeHolder") {
+			Vector3 temp = target.transform.position;
+			temp.x = lastPipesX + distance;
+			temp.y = Random.Range (pipeMin, pipeMax);
+			target.transform.position = temp;
+			lastPipesX = temp.x;
+		}
+	
+	}
+}
