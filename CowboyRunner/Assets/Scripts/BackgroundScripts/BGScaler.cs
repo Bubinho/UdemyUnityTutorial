04d5e1f5aace5116c9f379bf6eacd41232c10b87
Bubes <daniel-topp@t-online.de>
2017-10-06 12:29:23 +0200

    Cowboy Runner Project
    Added Background Movement
    Added Player Jump

---
+++ CowboyRunner/Assets/Scripts/BackgroundScripts/BGScaler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// class to supprt different rersaolution for backgrounds
+public class BGScaler : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		float height = Camera.main.orthographicSize * 2f; // camera height
+		float aspectRation_Screen = (float)Screen.width / Screen.height;
+		float width = height * aspectRation_Screen; // camera width
+
+
+		if (gameObject.name == "Background") {
+			transform.localScale = new Vector3 (width, height, 0);
+		} else {
+			transform.localScale = new Vector3 (width + 3 , 5, 0); // add a little bit to width to spawn enemys here
+		}
+	}
+
+}
---
+++ CowboyRunner/Assets/Scripts/Player Scripts/JumpPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class JumpPlayer : MonoBehaviour {
+
+	[SerializeField]
+	private AudioClip jumpClip;
+
+	[SerializeField]
+	private float jumpForce = 10f; 
+
+	[SerializeField]
+	private float forwardForce = 0f;
+		
+	private Rigidbody2D rb;
+	private bool canJump;
+	private Button jumpBtn;
+
+	// Use this for initialization
+	void Awake () {
+		rb = GetComponent<Rigidbody2D> ();
+		jumpBtn = GameObject.Find ("Jump Button").GetComponent<Button> ();
+
+		jumpBtn.onClick.AddListener (() => Jump());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Mathf.Abs (rb.velocity.y) == 0) {
+			canJump = true;
+		}
+	}
+
+	public void Jump(){
+		if(canJump){
+			canJump = false;
+
+			//AudioSource.PlayClipAtPoint (jumpClip, transform.position);
+
+			// if player is not in middle of the screen add forward movement
+			if (transform.position.x < 0) {
+				forwardForce = 2f;
+			} else {
+				forwardForce = 0f;
+			}
+
+			rb.velocity = new Vector2 (forwardForce, jumpForce);
+			
+		}
+	}
+}
